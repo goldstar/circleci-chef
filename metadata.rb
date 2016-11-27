9856360aec393e563d73abb9ed7a944dba8ffd5e
Joe Stump <joe@joestump.net>
2016-11-26 14:35:29 -1000

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'circleci'
+maintainer       'Joe Stump'
+maintainer_email 'jstump@goldstar.com'
+license          'Apache 2.0'
+description      'Installs/Configures circleci'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
