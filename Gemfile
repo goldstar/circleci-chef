80532e3c2cc104d90648a7ca806f1f7da0f82d22
Joe Stump <joe@joestump.net>
2016-11-27 09:14:14 -1000

    Add a stubbed cookbook for testing purposes.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org' do
   gem 'thor', '0.19.1'
   gem 'circleci'
   gem 'test-kitchen'
+  gem 'kitchen-docker'
   gem 'chefspec'
   gem 'foodcritic'
 end
---
+++ test/cookbooks/circleci_test/metadata.rb
@@ -0,0 +1,9 @@
+name             'circleci_test'
+maintainer       'Goldstar Events, Inc.'
+maintainer_email 'jstump@goldstar.com'
+license          'Apache 2.0'
+description      'Installs/Configures circleci_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'circleci'
