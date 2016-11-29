f67ef9cebca97d32517fed2a2fe703041d70e1ee
Joe Stump <jstump@goldstar.com>
2016-11-29 12:33:11 -0800

    Those stubs aren't needed for this test.

--- spec/unit/artifact_spec.rb
+++ spec/unit/artifact_spec.rb
@@ -14,10 +14,12 @@ describe 'circleci_artifact' do
   end
 
   it 'does nothing if artifact exists locally' do
-    allow_any_instance_of(CircleciCookbook::Helpers).to receive(:get_artifacts).and_return([])
-    allow_any_instance_of(CircleciCookbook::Helpers).to receive(:get_artifact_to_download).and_return({'url': 'https://example.com/path/to/artifact'})
     allow(File).to receive(:file?).and_return(true)
     chef_run.converge('circleci_test::default')
     expect(chef_run).to_not create_remote_file('/tmp/download.zip')
   end
 end
+
+# allow_any_instance_of(CircleciCookbook::Helpers).to receive(:get_artifacts).and_return([])
+# allow_any_instance_of(CircleciCookbook::Helpers).to receive(:get_artifact_to_download).and_return({'url': 'https://example.com/path/to/artifact'})
+
